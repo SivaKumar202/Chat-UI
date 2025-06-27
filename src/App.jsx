fafe57d07533fb3cc5ad11d881cc05b108dc51d0
SivaKumar202 <sivakumarjujjavarapu123@gmail.com>
2025-06-27 14:48:25 -0500

    Theme is setUp

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import myTheme from './Themes/Theme'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import { Button } from '@mui/material'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={myTheme}>
+      <CssBaseline/>
     <h1>chat app</h1>
-    </>
+    <Button variant="contained">Text</Button>
+    </ThemeProvider>
   )
 }
 
